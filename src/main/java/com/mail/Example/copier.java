0687f2ead100184144e1dc88816f07846f50644b
XiaoHang <1083594261@1-PC>
2017-09-15 17:01:56 +0800

    null

--- src/main/java/com/mail/Example/copier.java
+++ src/main/java/com/mail/Example/copier.java
@@ -17,26 +17,23 @@ import com.main.Authenticator.UserInfoUitl;
 
 public class copier {
 	public static void main(String[] args) throws MessagingException {
-		test1();
+		getMailFileList();
 	    }
-	public static void test(){
+	/**
+	 * 移动收件箱的邮件到指定的文件夹里
+	 * TODO ：未通过测试
+	 */
+	public static void getMailFileList(){
 			boolean debug = true;	
 			try {
 				//利用imap协议的新浪邮箱地址构造出url
 			    URLName url = new URLName("imap://imap.163.com");
-			    
 			    //不知道这是什么。。。权且记录
 			    Properties props=System.getProperties();
 			    props.setProperty("mail.imap.partialfetch", "false");
 			    props.setProperty("mail.imap.auth", "true");
-			    
-			    
-			    
-			    
-			    
 			    //根据系统属性集获取对话session。。TODO 不怎么理解为什么要用系统的属性集合来获取，我打印了一下，里面也没什么有用的属性啊
 			    Session session = Session.getInstance(props);
-			   
 			     //设置程序运行的日志输出，如果设置为false，则不会有任何日志输出，否则会输出debug信息
 			    session.setDebug(debug);  
 			    //根据url获取Store对象
@@ -45,7 +42,6 @@ public class copier {
 			    System.out.println("正在连接IMG......");
 			    store.connect(UserInfoUitl.getUserName(), UserInfoUitl.getpassword());
 			    System.out.println("成功连接");
-			    
 			    //获取邮箱里面的文件夹实例
 			    Folder sourceFolder=store.getFolder("INBOX");
 			    //以只读方式打开该文件夹
@@ -69,43 +65,14 @@ public class copier {
 			   sourceFolder.close(false);
 		    	store.close();
 		    	System.out.println("成功完成，关闭资源。。。。。。");
-			    
-			    
-			    
 			    /*Message  message=folder.getMessage(1);
-			    
 			    //注：如果该条信息里面的格式时text/plan格式的话，获取的该对象就是一个String对象，否则，就是一个MimeMultipart对象
 			    //解析起来草麻烦，不过，这次的目标仅仅是移动文件
 			    MimeMultipart mimeMultipart=(MimeMultipart)message.getContent();
-			    
 			    System.out.println(mimeMultipart.getBodyPart(1).getContent());*/
-			    
 			} catch (Exception e) {
 			    e.printStackTrace();
 			}
-	
 			System.exit(0);
 	}
-	public static  void test1() throws MessagingException{
-		// 准备连接服务器的会话信息  
-        Properties props = new Properties();  
-        props.setProperty("mail.store.protocol", "imap");  
-        props.setProperty("mail.imap.host", "imap.163.com");  
-        props.setProperty("mail.imap.port", "143");  
-          
-        // 创建Session实例对象  
-        Session session = Session.getInstance(props);  
-          
-        // 创建IMAP协议的Store对象  
-        Store store = session.getStore("imap");  
-          
-        // 连接邮件服务器  
-        store.connect(UserInfoUitl.getUserName(), UserInfoUitl.getpassword());  
-          
-        // 获得收件箱  
-        Folder folder = store.getFolder("INBOX");  
-        // 以读写模式打开收件箱  
-        folder.open(Folder.READ_WRITE); 
-	}
-	
-	}
+}
